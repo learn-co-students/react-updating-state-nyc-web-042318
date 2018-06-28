133cdee1ec78bbd9db2162425b25219bb74c6baf
Canino Developments <ian.canino@gmail.com>
2018-06-28 00:46:00 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+// src/index.js
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import ClickityClick from './components/ClickityClick'
 
 ReactDOM.render(
-  <div>
-    Mount Components Here
-  </div>,
+  <ClickityClick />,
   document.getElementById('root')
-);
+)
